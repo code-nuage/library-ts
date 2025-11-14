9e32ae0bab256397858552e460a1bd107a007a9f
code-nuage <eloi.hariot@protonmail.com>
2025-11-14 14:23:58 +0100

    Feat: Library

---
+++ src/library.ts
@@ -0,0 +1,20 @@
+class Library {
+    private books: Book[];
+
+    constructor() {}
+
+    addBook(b: Book): Library {
+        this.books.push(this.books);
+        return this;
+    }
+
+    listAvailable(): Book[] {
+        return this.books.filter(b => b.available === true);
+    }
+
+    findBookById(id: number): Book[] | undefined {
+        return this.books.find(b => b.id === id);
+    }
+}
+
+export default Library;
