9820e403ab44ce35c4fbed677ff34d37030e8f16
Matis <matisconan2006@gmail.com>
2025-11-14 14:15:37 +0100

    Add: Class User

---
+++ src/user.ts
@@ -0,0 +1,20 @@
+class User {
+  constructor(public firstname: string, public lastname: string, private _age: number) {
+  }
+
+  getFullName(): string {
+    return `${this.firstname} ${this.lastname}`;
+  }
+
+  get age(): number {
+    return this._age;
+  }
+
+  set age(value: number) {
+    if (value >= 0) {
+      this._age = value;
+    }
+  }
+}
+
+export = User;
